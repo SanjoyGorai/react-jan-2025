9f55ff6e76fcf31f323deb2e9864c3a70bdc83f8
Sanjoy Gorai <sanjoy10062000@gmail.com>
2025-01-01 09:16:58 +0530

    basic style added

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,36 @@
 
 function App() {
   return (
-    <div className="flex justify-center items-center ">
-      <div className="flex flex-col bg-blue-400 m-1 rounded-t-md">
+    <div className="lg:flex justify-center items-center">
+      <div className="flex flex-col bg-blue-400 m-1 rounded-t-md hover:cursor-pointer hover:bg-fuchsia-500">
         <img
           src="../src/assets/images/sanjoy6.jpg"
           alt="sanjoy6"
-          className="w-72  "
+          className="w-72 hover:opacity-80"
         />
-        <p className="text-white font-semibold p-1">Sanjoy Gorai</p>
+        <p className="text-white font-semibold p-1 ">Sanjoy Gorai</p>
       </div>
-      <div className="flex flex-col  bg-blue-400 m-1">
+      <div className="flex flex-col  bg-blue-400 m-1 hover:cursor-pointer hover:bg-fuchsia-500">
         <img
           src="../src/assets/images/grany1.jpg"
           alt="sanjoy6"
-          className="w-72 "
+          className="w-72 hover:opacity-80"
         />
         <p className=" text-white font-semibold p-1">Sankari Gorai</p>
       </div>
-      <div className="flex flex-col  bg-blue-400 m-1">
+      <div className="flex flex-col  bg-blue-400 m-1 hover:cursor-pointer hover:bg-fuchsia-500">
         <img
           src="../src/assets/images/dad.jpg"
           alt="sanjoy6"
-          className="w-72 "
+          className="w-72 hover:opacity-80"
         />
         <p className=" text-white font-semibold p-1">Sunil Gorai</p>
       </div>
-      <div className="flex flex-col  bg-blue-400 m-1">
+      <div className="flex flex-col  bg-blue-400 m-1 hover:cursor-pointer hover:bg-fuchsia-500">
         <img
           src="../src/assets/images/sanjoy1.jpg"
           alt="sanjoy6"
-          className="w-72 "
+          className="w-72 hover:opacity-80"
         />
         <p className=" text-white font-semibold p-1">Mithu Gorai</p>
       </div>
